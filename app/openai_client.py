58314dc7d409f98c3351c3d2bfca4ef996361943
Pedro Fioravanti <pedrofioravanti@usp.br>
2025-12-04 09:56:14 -0300

    basic wrapper created

---
+++ app/openai_client.py
@@ -0,0 +1,24 @@
+import os
+import openai
+from dotenv import load_dotenv
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# AI Models
+EMBEDDING_MODEL = "gpt-4o-mini-embed"
+LLM_MODEL = "gpt-4o-mini"
+
+def create_embedding(text: str):
+    response = openai.Embedding.create(model=EMBEDDING_MODEL, input=text)
+    return resp["data"][0]["embedding"]
+
+def chat_with_messages(messages, max_tokens=512, temperature=0.2):
+    resp = openai.ChatCompletion.create(
+        MODEL = LLM_MODEL,
+        messages=messages,
+        max_tokens=max_tokens,
+        temperature=temperature
+    )
+
+return resp['choices'][0]['message']['content']
